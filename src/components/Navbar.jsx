4a2426496bdb16568833cc1397bdfd0a567f6869
BroKarim <146515339+BroKarim@users.noreply.github.com>
2023-10-01 13:53:36 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -53,10 +53,8 @@ export default function Navbar() {
           <div className="fixed inset-0 z-50" onClick={toggleMobileMenu}></div>
           <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
-                {/* logo di mobile */}
-                <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+              <a href="#" className="-m-1.5 p-1.5 font-fredoka font-medium">
+                Richard
               </a>
               <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" onClick={toggleMobileMenu}>
                 <span className="sr-only">Toggle mobile menu</span>
